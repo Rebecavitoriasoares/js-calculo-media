46b0de2dd0393b61ee71a4de08263e71f7d96907
Rebeca Soares <112526287+Rebecavitoriasoares@users.noreply.github.com>
2022-10-18 21:06:08 -0300

    programa de média
    fizemos calculos no visual studio

---
+++ app.js
@@ -0,0 +1,15 @@
+ //  console.log("Olá, estou funcionando")
+ //  console.log(240*4)
+ //  console.log(200+450)
+
+let nome = "Rebeca"
+let nota1 = 9
+let nota2 = 8 
+let nota3 = 7
+let nota4 = 5
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+let mediaFixada = media.toFixed (1)
+console.log("Olá" + nome)
+console.log("Sua media foi:" + médiaFixada)
+
+
